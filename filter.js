1fa7896144a91785f30b7ec7bdf889bec220f5d7
John Shields <shields101@hotmail.com>
2024-02-06 19:25:12 +0000

    Lesson complete

--- filter.js
+++ filter.js
@@ -21,6 +21,11 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(person => person.name === 'Paul');
+console.log(paul);
 
 // Complex Filtering
 const students = [
@@ -55,3 +60,8 @@ const students = [
     ]
   },
 ];
+
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0; //greater than 0 as 0 will still be returned for above results
+const candidates = students.filter(hasStrongSkills);
+console.log(candidates);
